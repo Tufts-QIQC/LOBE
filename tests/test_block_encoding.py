228bbc3014d2cf8ba05a9feb0bb6543b2b0bed2e
William Simon <simonwa7@gmail.com>
2024-07-29 15:22:59 -0400

    Removing unused code

--- tests/test_block_encoding.py
+++ tests/test_block_encoding.py
@@ -47,14 +47,12 @@ import pytest
     ],
 )
 @pytest.mark.parametrize("maximum_occupation_number", [1, 3])
-@pytest.mark.parametrize("decompose", [False, True])
-def test_lobe_block_encoding(
+def test_lobe_block_encoding_undecomposed(
     terms,
     has_bosons,
     has_fermions,
     has_antifermions,
     maximum_occupation_number,
-    decompose,
 ):
     hamiltonian = terms[0]
     for term in terms[1:]:
@@ -115,7 +113,110 @@ def test_lobe_block_encoding(
         rotation_qubits,
         clean_ancillae,
         perform_coefficient_oracle=True,
-        decompose=decompose,
+        decompose=False,
+    )
+    circuit += add_naive_usp(index_register)
+
+    upper_left_block = circuit.unitary(dtype=complex)[
+        : 1 << system.number_of_system_qubits, : 1 << system.number_of_system_qubits
+    ]
+
+    assert np.allclose(upper_left_block * block_encoding_scaling_factor, matrix)
+
+
+@pytest.mark.parametrize(
+    "terms, has_bosons, has_fermions, has_antifermions",
+    [
+        (
+            [
+                ParticleOperator("a0"),
+                ParticleOperator("a1"),
+                ParticleOperator("a0^ a1"),
+                ParticleOperator("a1^ a0"),
+            ],
+            True,
+            False,
+            False,
+        ),
+        (
+            [
+                ParticleOperator("a0^ b0"),
+                0.25 * ParticleOperator("b0^ a0"),
+            ],
+            True,
+            True,
+            False,
+        ),
+    ],
+)
+@pytest.mark.parametrize("maximum_occupation_number", [1, 3])
+def test_lobe_block_encoding_decomposed(
+    terms,
+    has_bosons,
+    has_fermions,
+    has_antifermions,
+    maximum_occupation_number,
+):
+    hamiltonian = terms[0]
+    for term in terms[1:]:
+        hamiltonian += term
+
+    number_of_modes = max([term.max_mode() for term in terms]) + 1
+
+    full_fock_basis = get_basis_of_full_system(
+        number_of_modes,
+        maximum_occupation_number,
+        has_fermions=has_fermions,
+        has_antifermions=has_antifermions,
+        has_bosons=has_bosons,
+    )
+    matrix = op.generate_matrix_from_basis(hamiltonian, full_fock_basis)
+
+    rescaled_terms, scaling_factor = rescale_terms(terms, maximum_occupation_number)
+
+    max_number_of_bosonic_ops_in_term = max(
+        get_numbers_of_bosonic_operators_in_terms(terms)
+    )
+
+    number_of_ancillae = 5
+    number_of_index_qubits = max(int(np.ceil(np.log2(len(terms)))), 1)
+    number_of_rotation_qubits = max_number_of_bosonic_ops_in_term + 1
+
+    block_encoding_scaling_factor = (1 << number_of_index_qubits) * scaling_factor
+
+    # Declare Qubits
+    circuit = cirq.Circuit()
+    validation = cirq.LineQubit(0)
+    clean_ancillae = [cirq.LineQubit(i + 1) for i in range(number_of_ancillae)]
+    rotation_qubits = [
+        cirq.LineQubit(i + 1 + number_of_ancillae)
+        for i in range(number_of_rotation_qubits)
+    ]
+    index_register = [
+        cirq.LineQubit(i + 1 + number_of_ancillae + 3)
+        for i in range(number_of_index_qubits)
+    ]
+    system = System(
+        number_of_modes=number_of_modes,
+        maximum_occupation_number=maximum_occupation_number,
+        number_of_used_qubits=1 + number_of_ancillae + 3 + number_of_index_qubits,
+        has_fermions=has_fermions,
+        has_antifermions=has_antifermions,
+        has_bosons=has_bosons,
+    )
+
+    # Generate full Block-Encoding circuit
+    circuit.append(cirq.X.on(validation))
+    circuit += add_naive_usp(index_register)
+    circuit += add_lobe_oracle(
+        rescaled_terms,
+        validation,
+        index_register,
+        system,
+        rotation_qubits,
+        clean_ancillae,
+        perform_coefficient_oracle=True,
+        decompose=True,
     )
     circuit += add_naive_usp(index_register)
 
