68f5461905ced23bf749939a7d9a3efa42a18e06
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2024-12-12 16:25:40 -0500

    Remove Identity op and skip large tests

--- tests/test_lcu.py
+++ tests/test_lcu.py
@@ -14,7 +14,14 @@ def test_lcu_circuit_block_encodes_random_ParticleOperator(
     op = ParticleOperator.random(
         n_terms=n_terms, max_len_of_terms=max_len_of_terms, max_mode=2
     )
-    qubit_op = op_qubit_map(op, max_bose_occ=max_bose_occ)
 
-    lcu = LCU(op, max_bose_occ=max_bose_occ)
-    assert np.allclose(lcu.unitary, qubit_op.to_sparse_matrix.toarray())
+    if () not in list(op.op_dict.keys()):
+        qubit_op = op_qubit_map(op, max_bose_occ=max_bose_occ)
+
+        lcu = LCU(op, max_bose_occ=max_bose_occ)
+
+        if len(lcu.get_circuit().all_qubits()) >= 14:
+            pytest.skip(
+                f"too many qubits {len(lcu.all_qubits())} to explicitly validate"
+            )
+        assert np.allclose(lcu.unitary, qubit_op.to_sparse_matrix.toarray())
