d58dd9eed6a6c8699db82595e288bb784af27aa3
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2024-12-20 14:04:07 -0500

    Implement and test a^\dagger^R a^S

--- src/lobe/bosonic.py
+++ src/lobe/bosonic.py
@@ -1,3 +1,82 @@
+from .metrics import CircuitMetrics
+import cirq
+import numpy as np
+from src.lobe.addition import add_classical_value_gate_efficient
+from src.lobe.multiplexed_rotations import get_decomposed_multiplexed_rotation_circuit
+
+
+def _add_multi_bosonic_rotations(
+    rotation_qubit,
+    bosonic_mode_register,
+    creation_exponent=0,
+    annihilation_exponent=0,
+    clean_ancillae=[],
+    ctrls=([], []),
+    numerics=None,
+):
+    """Add rotations to pickup bosonic coefficients corresponding to a series of ladder operators (assumed
+        to be normal ordered) acting on one bosonic mode within a term.
+
+    Args:
+        rotation_qubit (cirq.LineQubit): The qubit that is rotated to pickup the amplitude corresponding
+            to the coefficients that appear when a bosonic op hits a quantum state
+        bosonic_mode_register (List[cirq.LineQubit]): The qubits that store the occupation of the bosonic
+            mode being acted upon.
+        creation_exponent (int): The number of subsequent creation operators in the term
+        annihilation_exponent (int): The number of subsequent annihilation operators in the term
+        ctrls (Tuple(List[cirq.LineQubit], List[int])): A set of qubits and integers that correspond to
+            the control qubits and values.
+
+    Returns:
+        - The gates to perform the unitary operation
+    """
+    gates = []
+
+    maximum_occupation_number = (1 << len(bosonic_mode_register)) - 1
+
+    # Flip the rotation qubit outside the encoded subspace
+    gates.append(
+        cirq.Moment(
+            cirq.X.on(rotation_qubit).controlled_by(*ctrls[0], control_values=ctrls[1])
+        )
+    )
+
+    # Multiplexing over computational basis states of mode register that will not be zeroed-out
+    angles = []
+    for particle_number in range(
+        0,
+        maximum_occupation_number + 1,
+    ):
+        if (particle_number - creation_exponent) < 0:
+            angles.append(0)
+        elif (
+            particle_number - creation_exponent + annihilation_exponent
+        ) > maximum_occupation_number:
+            angles.append(0)
+        else:
+            # Classically compute coefficient that should appear
+            intended_coefficient = 1
+            for power in range(creation_exponent):
+                intended_coefficient *= np.sqrt(
+                    (particle_number - power) / (maximum_occupation_number + 1)
+                )
+            for power in range(annihilation_exponent):
+                intended_coefficient *= np.sqrt(
+                    (particle_number - creation_exponent + power + 1)
+                    / (maximum_occupation_number + 1)
+                )
+            angles.append(2 * np.arcsin(-1 * intended_coefficient) / np.pi)
+
+    gates += get_decomposed_multiplexed_rotation_circuit(
+        bosonic_mode_register + [rotation_qubit],
+        angles,
+        clean_ancillae=clean_ancillae,
+        ctrls=ctrls,
+        numerics=numerics,
+    )
+    return gates
+
+
 def bosonic_mode_block_encoding(
     system,
     block_encoding_ancilla,
@@ -23,7 +102,33 @@ def bosonic_mode_block_encoding(
         - List of cirq operations representing the gates to be applied in the circuit
         - CircuitMetrics object representing cost of block-encoding circuit
     """
-    pass
+
+    gates = []
+    block_encoding_metrics = CircuitMetrics()
+
+    # gates.append(cirq.H.on(block_encoding_ancilla))
+    R, S = exponents[0], exponents[1]
+
+    adder_controls = (ctrls[0] + [block_encoding_ancilla], ctrls[1] + [0])
+    gates += add_classical_value_gate_efficient(
+        system.bosonic_system[active_index],
+        R - S,
+        clean_ancillae=clean_ancillae,
+        ctrls=adder_controls,
+    )
+
+    gates += _add_multi_bosonic_rotations(
+        block_encoding_ancilla,
+        system.bosonic_system[active_index],
+        R,
+        S,
+        clean_ancillae=clean_ancillae,
+        ctrls=ctrls,
+    )
+
+    # gates.append(cirq.H.on(block_encoding_ancilla))
+
+    return gates, block_encoding_metrics
 
 
 def bosonic_mode_plus_hc_block_encoding(
