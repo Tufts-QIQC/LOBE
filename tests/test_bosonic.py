a900d1f4943ea2df57b34921d8abcb37ade46732
Carter M. Gustin <83713139+cgustin99@users.noreply.github.com>
2025-01-20 09:47:01 -0500

    Test block encoding for individual term in Phi4 Hamiltonian

--- tests/test_bosonic.py
+++ tests/test_bosonic.py
@@ -1,5 +1,7 @@
-from openparticle import ParticleOperator
+from openparticle import ParticleOperator, generate_matrix
 import numpy as np
+import cirq
+from src.lobe.system import System
 from src.lobe.bosonic import (
     bosonic_mode_block_encoding,
     bosonic_modes_block_encoding,
@@ -8,12 +10,18 @@ from src.lobe.bosonic import (
 )
 import pytest
 from functools import partial
+
 from _utils import (
     _setup,
     _validate_block_encoding,
     _validate_clean_ancillae_are_cleaned,
     _validate_block_encoding_does_nothing_when_control_is_off,
+    get_basis_of_full_system,
 )
+from src.lobe._utils import get_bosonic_exponents
+
+from src.lobe.rescale import bosonically_rescale_terms
+from src.lobe.asp import get_target_state
 
 
 @pytest.mark.parametrize("number_of_modes", range(2, 4))
@@ -302,3 +310,163 @@ def test_bosonic_product_plus_hc_block_encoding(
         maximum_occupation_number + 3
     )
     assert metrics.clean_ancillae_usage[-1] == 0
+
+
+@pytest.mark.parametrize(
+    "term",
+    [
+        ParticleOperator("a1^ a1^ a1 a1"),
+        ParticleOperator("a1^ a0^ a0 a1"),
+        ParticleOperator("a1^ a1"),
+        ParticleOperator("a0^ a0^ a0 a0"),
+        ParticleOperator("a0^ a0"),
+    ],
+)
+def test_phi4_individual_term(term):
+    max_bose_occ = 3
+    active_modes, exponents = get_bosonic_exponents(term, term.max_bosonic_mode + 1)
+
+    terms = term.to_list()
+
+    bosonically_rescaled_terms, bosonic_rescaling_factor = bosonically_rescale_terms(
+        terms, max_bose_occ
+    )
+    coefficients = [term.coeff for term in bosonically_rescaled_terms]
+
+    norm = sum(np.abs(coefficients))
+    target_state = get_target_state(coefficients)
+    asp_rescaling_factor = bosonic_rescaling_factor * norm
+
+    number_of_modes = max([term.max_mode for term in terms]) + 1
+
+    number_of_ancillae = (
+        1000  # Some arbitrary large number with most ancilla disregarded
+    )
+    number_of_index_qubits = max(int(np.ceil(np.log2(len(terms)))), 1)
+    number_of_rotation_qubits = len(active_modes)
+
+    # Declare Qubits
+    control = cirq.LineQubit(0)
+    clean_ancillae = [cirq.LineQubit(i + 1) for i in range(number_of_ancillae)]
+
+    block_encoding_ancillae = [
+        cirq.LineQubit(number_of_index_qubits + i + number_of_ancillae)
+        for i in range(number_of_rotation_qubits + 1)
+    ]  # Index register + rotation qubits
+
+    system = System(
+        number_of_modes=number_of_modes,
+        maximum_occupation_number=max_bose_occ,
+        number_of_used_qubits=1
+        + number_of_ancillae
+        + number_of_rotation_qubits
+        + number_of_index_qubits,
+        has_fermions=term.has_fermions,
+        has_antifermions=term.has_antifermions,
+        has_bosons=term.has_bosons,
+    )
+    gates = []
+    gates.append(cirq.X.on(control))
+    _gates, metrics = bosonic_modes_block_encoding(
+        system,
+        block_encoding_ancillae,
+        active_modes,
+        exponents,
+        clean_ancillae=clean_ancillae,
+        ctrls=([control], [1]),
+    )
+    gates += _gates
+    gates.append(cirq.X.on(control))
+
+    unitary_rescaling_factor = 2 * np.sqrt(max_bose_occ) ** sum(
+        sum(np.asarray(exponents))
+    )
+
+    unitary = (
+        cirq.Circuit(gates).unitary()[
+            : 1 << system.number_of_system_qubits, : 1 << system.number_of_system_qubits
+        ]
+        * unitary_rescaling_factor
+    )
+    basis = get_basis_of_full_system(2, max_bose_occ, False, False, True)
+    matrix = generate_matrix(term + term.dagger(), basis)
+
+
+@pytest.mark.parametrize(
+    "term",
+    [
+        ParticleOperator("a1^ a1^ a1 a1"),
+        ParticleOperator("a1^ a0^ a0 a1"),
+        ParticleOperator("a1^ a1"),
+        ParticleOperator("a0^ a0^ a0 a0"),
+        ParticleOperator("a0^ a0"),
+    ],
+)
+def test_phi4_individual_term_plus_hc(term):
+    max_bose_occ = 3
+    active_modes, exponents = get_bosonic_exponents(term, term.max_bosonic_mode + 1)
+
+    terms = term.to_list()
+
+    bosonically_rescaled_terms, bosonic_rescaling_factor = bosonically_rescale_terms(
+        terms, max_bose_occ
+    )
+    coefficients = [term.coeff for term in bosonically_rescaled_terms]
+
+    norm = sum(np.abs(coefficients))
+    target_state = get_target_state(coefficients)
+    asp_rescaling_factor = bosonic_rescaling_factor * norm
+
+    number_of_modes = max([term.max_mode for term in terms]) + 1
+
+    number_of_ancillae = (
+        1000  # Some arbitrary large number with most ancilla disregarded
+    )
+    number_of_index_qubits = max(int(np.ceil(np.log2(len(terms)))), 1)
+    number_of_rotation_qubits = len(active_modes)
+
+    # Declare Qubits
+    control = cirq.LineQubit(0)
+    clean_ancillae = [cirq.LineQubit(i + 1) for i in range(number_of_ancillae)]
+
+    block_encoding_ancillae = [
+        cirq.LineQubit(number_of_index_qubits + i + number_of_ancillae)
+        for i in range(number_of_rotation_qubits + 1)
+    ]  # Index register + rotation qubits
+
+    system = System(
+        number_of_modes=number_of_modes,
+        maximum_occupation_number=max_bose_occ,
+        number_of_used_qubits=1
+        + number_of_ancillae
+        + number_of_rotation_qubits
+        + number_of_index_qubits,
+        has_fermions=term.has_fermions,
+        has_antifermions=term.has_antifermions,
+        has_bosons=term.has_bosons,
+    )
+    gates = []
+    gates.append(cirq.X.on(control))
+    _gates, metrics = bosonic_product_plus_hc_block_encoding(
+        system,
+        block_encoding_ancillae,
+        active_modes,
+        exponents,
+        clean_ancillae=clean_ancillae,
+        ctrls=([control], [1]),
+    )
+    gates += _gates
+    gates.append(cirq.X.on(control))
+
+    unitary_rescaling_factor = 2 * np.sqrt(max_bose_occ) ** sum(
+        sum(np.asarray(exponents))
+    )
+
+    unitary = (
+        cirq.Circuit(gates).unitary()[
+            : 1 << system.number_of_system_qubits, : 1 << system.number_of_system_qubits
+        ]
+        * unitary_rescaling_factor
+    )
+    basis = get_basis_of_full_system(2, max_bose_occ, False, False, True)
+    matrix = generate_matrix(term + term.dagger(), basis)
