Fixing computation of rescaling factor; small aesthetic changes

src/lobe/hamiltonian_metrics.py
@@ -1,24 +1,18 @@
 import cirq
 import numpy as np
-from openparticle import ParticleOperator
-from copy import deepcopy
 
-from .addition import _get_p_val, add_classical_value
-from .bosonic import bosonic_product_block_encoding, _get_bosonic_rotation_angles
-from .fermionic import fermionic_product_block_encoding
-from .interaction import interaction_term_block_encoding
+from .addition import add_classical_value
+from .bosonic import _get_bosonic_rotation_angles
 from .metrics import CircuitMetrics
 from .multiplexed_rotations import _process_rotation_angles
-from .system import System
-
 from ._utils import (
-    get_fermionic_operator_types, 
-    get_bosonic_exponents, 
-    translate_antifermions_to_fermions, 
-    get_active_bosonic_modes, 
+    get_bosonic_exponents,
+    translate_antifermions_to_fermions,
+    get_active_bosonic_modes,
     get_active_fermionic_modes,
-    predict_number_of_block_encoding_ancillae
+    predict_number_of_block_encoding_ancillae,
 )
+
 
 def _compute_n_elbows_and_anc_hw_per_incrementer(N, m):
     """
@@ -27,16 +21,14 @@ def _compute_n_elbows_and_anc_hw_per_incrementer(N, m):
         number of elbows and max clean ancillae to implement this operation
     """
     control = [cirq.LineQubit(0)]
-    clean_ancillae = [cirq.LineQubit(i) for i in range(-1, -100, -1)]  
+    clean_ancillae = [cirq.LineQubit(i) for i in range(-1, -100, -1)]
 
     main_register = [cirq.LineQubit(i) for i in range(1, N + 1)]
     _, metrics = add_classical_value(
-        main_register,
-        m,
-        clean_ancillae=clean_ancillae,
-        ctrls = (control, [1])
+        main_register, m, clean_ancillae=clean_ancillae, ctrls=(control, [1])
     )
     return metrics.number_of_elbows, metrics.ancillae_highwater()
+
 
 def count_metrics_analytic(operator, max_occupancy: int = 1):
     """
@@ -72,51 +64,79 @@ def count_metrics_analytic(operator, max_occupancy: int = 1):
             active_bosonic_modes, exponents_list = get_bosonic_exponents(term)
             P = sum([exponents[0] + exponents[1] for exponents in exponents_list])
 
-            if term.has_fermions and not term.has_bosons: #bi^ bi, bi^ bi bj^ bj, ...
-                rescaling_factor += 1
+            if term.has_fermions and not term.has_bosons:  # bi^ bi, bi^ bi bj^ bj, ...
+                rescaling_factor += 1 * np.abs(term.coeffs[0])
                 number_of_elbows = len(active_fermionic_modes)
-                clean_ancillae_usage = [len(active_fermionic_modes) + number_of_indexing_clean_ancillae]
-                rotation_angles = []
-            elif term.has_bosons and not term.has_fermions: #ai^ ai, ai^ ai aj^ aj, ...
-                rescaling_factor += max_occupancy  ** (P/2)
-                number_of_elbows = int(np.ceil(np.log2(max_occupancy + 1))) * len((active_bosonic_modes))
                 clean_ancillae_usage = [
-                    i + number_of_indexing_clean_ancillae for i in range(1, 
-                                            int(np.ceil(np.log2(max_occupancy + 1))) + 1)
+                    len(active_fermionic_modes) + number_of_indexing_clean_ancillae
+                ]
+                rotation_angles = []
+            elif (
+                term.has_bosons and not term.has_fermions
+            ):  # ai^ ai, ai^ ai aj^ aj, ...
+                rescaling_factor += np.abs(term.coeffs[0]) * max_occupancy ** (P / 2)
+                number_of_elbows = int(np.ceil(np.log2(max_occupancy + 1))) * len(
+                    (active_bosonic_modes)
+                )
+                clean_ancillae_usage = [
+                    i + number_of_indexing_clean_ancillae
+                    for i in range(1, int(np.ceil(np.log2(max_occupancy + 1))) + 1)
                 ]
                 rotation_angles = []
                 for exponents in exponents_list:
-                    angles = _get_bosonic_rotation_angles(maximum_occupation_number=max_occupancy,
-                                                                creation_exponent=exponents[0],
-                                                                annihilation_exponent=exponents[1])
-                    angles = np.concatenate(
-                        [angles, np.zeros((1 << int(np.ceil(np.log2(len(angles))))) - len(angles))]
+                    angles = _get_bosonic_rotation_angles(
+                        maximum_occupation_number=max_occupancy,
+                        creation_exponent=exponents[0],
+                        annihilation_exponent=exponents[1],
                     )
-                    processed_angles = list(_process_rotation_angles(angles))
-                    rotation_angles += processed_angles + [-sum(processed_angles)/2, sum(processed_angles)/2]
-
-            else: #bi^ bi ai^ ai
-                rescaling_factor += max_occupancy  ** (P/2) 
-                clean_ancillae_usage = [len(active_fermionic_modes) + number_of_indexing_clean_ancillae] +\
+                    angles = np.concatenate(
                         [
-                    i + number_of_indexing_clean_ancillae for i in range(1, 
-                                            int(np.ceil(np.log2(max_occupancy + 1))) + 1)
+                            angles,
+                            np.zeros(
+                                (1 << int(np.ceil(np.log2(len(angles))))) - len(angles)
+                            ),
+                        ]
+                    )
+                    processed_angles = list(_process_rotation_angles(angles))
+                    rotation_angles += processed_angles + [
+                        -sum(processed_angles) / 2,
+                        sum(processed_angles) / 2,
+                    ]
+
+            else:  # bi^ bi ai^ ai
+                rescaling_factor += np.abs(term.coeffs[0]) * max_occupancy ** (P / 2)
+                clean_ancillae_usage = [
+                    len(active_fermionic_modes) + number_of_indexing_clean_ancillae
+                ] + [
+                    i + number_of_indexing_clean_ancillae
+                    for i in range(1, int(np.ceil(np.log2(max_occupancy + 1))) + 1)
                 ]
                 rotation_angles = []
                 for exponents in exponents_list:
-                    angles = _get_bosonic_rotation_angles(maximum_occupation_number=max_occupancy,
-                                                                creation_exponent=exponents[0],
-                                                                annihilation_exponent=exponents[1])
+                    angles = _get_bosonic_rotation_angles(
+                        maximum_occupation_number=max_occupancy,
+                        creation_exponent=exponents[0],
+                        annihilation_exponent=exponents[1],
+                    )
                     angles = np.concatenate(
-                        [angles, np.zeros((1 << int(np.ceil(np.log2(len(angles))))) - len(angles))]
+                        [
+                            angles,
+                            np.zeros(
+                                (1 << int(np.ceil(np.log2(len(angles))))) - len(angles)
+                            ),
+                        ]
                     )
                     processed_angles = list(_process_rotation_angles(angles))
-                    rotation_angles += processed_angles + [-sum(processed_angles)/2, sum(processed_angles)/2]
+                    rotation_angles += processed_angles + [
+                        -sum(processed_angles) / 2,
+                        sum(processed_angles) / 2,
+                    ]
 
-                number_of_elbows = (len(active_fermionic_modes)) +\
-                      int(np.ceil(np.log2(max_occupancy + 1))) * len(active_bosonic_modes)
+                number_of_elbows = (len(active_fermionic_modes)) + int(
+                    np.ceil(np.log2(max_occupancy + 1))
+                ) * len(active_bosonic_modes)
         elif len(term) > 1:
-            #term + h.c.
+            # term + h.c.
             term = term.to_list()[0]
 
             active_fermionic_modes = get_active_fermionic_modes(term)
@@ -124,58 +144,57 @@ def count_metrics_analytic(operator, max_occupancy: int = 1):
 
             # Determine rotations
             _, exponents_list = get_bosonic_exponents(term, term.max_mode + 1)
-            P = sum([exponents_list[i][0] + exponents_list[i][1] for i in range(len(exponents_list))])
-            RS_list = [exponents_list[i][0] - exponents_list[i][1] for i in range(len(exponents_list))]
-            
+            P = sum(
+                [
+                    exponents_list[i][0] + exponents_list[i][1]
+                    for i in range(len(exponents_list))
+                ]
+            )
+            RS_list = [
+                exponents_list[i][0] - exponents_list[i][1]
+                for i in range(len(exponents_list))
+            ]
+
             rotation_angles = []
             for exponents in exponents_list:
-                angles = _get_bosonic_rotation_angles(maximum_occupation_number=max_occupancy,
-                                                            creation_exponent=exponents[0],
-                                                            annihilation_exponent=exponents[1])
+                angles = _get_bosonic_rotation_angles(
+                    maximum_occupation_number=max_occupancy,
+                    creation_exponent=exponents[0],
+                    annihilation_exponent=exponents[1],
+                )
                 angles = np.concatenate(
-                    [angles, np.zeros((1 << int(np.ceil(np.log2(len(angles))))) - len(angles))]
+                    [
+                        angles,
+                        np.zeros(
+                            (1 << int(np.ceil(np.log2(len(angles))))) - len(angles)
+                        ),
+                    ]
                 )
                 processed_angles = list(_process_rotation_angles(angles))
-                rotation_angles += processed_angles + [-sum(processed_angles)/2, sum(processed_angles)/2]
-
-            if term.has_fermions and not term.has_bosons: #e.g. bi^ bj + bj^ bi
-                rescaling_factor += 1
-                clean_ancillae_usage = [len(active_fermionic_modes) - 1 + number_of_indexing_clean_ancillae]
-                number_of_elbows = len(active_fermionic_modes) - 1
-            
-            elif term.has_bosons and not term.has_fermions: #e.g. ai^ aj + aj^ ai
-                rescaling_factor +=  (max_occupancy ** (P / 2))
-                clean_ancillae_usage = [
-                    i + number_of_indexing_clean_ancillae for i in range(1, int(np.ceil(np.log2(max_occupancy + 1))) + 1 + 1)
+                rotation_angles += processed_angles + [
+                    -sum(processed_angles) / 2,
+                    sum(processed_angles) / 2,
                 ]
-                
-                number_of_elbows = 1 + len(active_bosonic_modes) * int(np.ceil(np.log2(max_occupancy + 1)))
 
-                for RminusS in RS_list: #+R - S
-                    elbows, clean_anc = _compute_n_elbows_and_anc_hw_per_incrementer(
-                        int(np.ceil(np.log2(max_occupancy + 1))), RminusS
-                    )
-                    number_of_elbows += elbows
-                    clean_ancillae_usage.append(clean_anc)
-                    elbows, clean_anc = _compute_n_elbows_and_anc_hw_per_incrementer(
-                        int(np.ceil(np.log2(max_occupancy + 1))), -RminusS
-                    )
-                    number_of_elbows += elbows
-                    clean_ancillae_usage.append(clean_anc)
-                    
-                    
-
-            else: # e.g. bi^ bj ak + h.c.
-                rescaling_factor += max_occupancy ** (P/2)
+            if term.has_fermions and not term.has_bosons:  # e.g. bi^ bj + bj^ bi
+                rescaling_factor += np.abs(term.coeffs[0]) * 1
                 clean_ancillae_usage = [
-                            i + number_of_indexing_clean_ancillae
-                            for i in range(
-                                1, int(np.ceil(np.log2(max_occupancy + 1))) + 1 + 1
-                            )
-                        ]
-                number_of_elbows = (len(active_fermionic_modes))  +\
-                             len(active_bosonic_modes) * int(np.ceil(np.log2(max_occupancy + 1)))
-                for RminusS in RS_list: #+R - S
+                    len(active_fermionic_modes) - 1 + number_of_indexing_clean_ancillae
+                ]
+                number_of_elbows = len(active_fermionic_modes) - 1
+
+            elif term.has_bosons and not term.has_fermions:  # e.g. ai^ aj + aj^ ai
+                rescaling_factor += np.abs(term.coeffs[0]) * (max_occupancy ** (P / 2))
+                clean_ancillae_usage = [
+                    i + number_of_indexing_clean_ancillae
+                    for i in range(1, int(np.ceil(np.log2(max_occupancy + 1))) + 1 + 1)
+                ]
+
+                number_of_elbows = 1 + len(active_bosonic_modes) * int(
+                    np.ceil(np.log2(max_occupancy + 1))
+                )
+
+                for RminusS in RS_list:  # +R - S
                     elbows, clean_anc = _compute_n_elbows_and_anc_hw_per_incrementer(
                         int(np.ceil(np.log2(max_occupancy + 1))), RminusS
                     )
@@ -186,14 +205,33 @@ def count_metrics_analytic(operator, max_occupancy: int = 1):
                     )
                     number_of_elbows += elbows
                     clean_ancillae_usage.append(clean_anc)
-        
+
+            else:  # e.g. bi^ bj ak + h.c.
+                rescaling_factor += np.abs(term.coeffs[0]) * max_occupancy ** (P / 2)
+                clean_ancillae_usage = [
+                    i + number_of_indexing_clean_ancillae
+                    for i in range(1, int(np.ceil(np.log2(max_occupancy + 1))) + 1 + 1)
+                ]
+                number_of_elbows = (len(active_fermionic_modes)) + len(
+                    active_bosonic_modes
+                ) * int(np.ceil(np.log2(max_occupancy + 1)))
+                for RminusS in RS_list:  # +R - S
+                    elbows, clean_anc = _compute_n_elbows_and_anc_hw_per_incrementer(
+                        int(np.ceil(np.log2(max_occupancy + 1))), RminusS
+                    )
+                    number_of_elbows += elbows
+                    clean_ancillae_usage.append(clean_anc)
+                    elbows, clean_anc = _compute_n_elbows_and_anc_hw_per_incrementer(
+                        int(np.ceil(np.log2(max_occupancy + 1))), -RminusS
+                    )
+                    number_of_elbows += elbows
+                    clean_ancillae_usage.append(clean_anc)
 
         metrics.number_of_elbows += number_of_elbows
         metrics.clean_ancillae_usage += clean_ancillae_usage
         metrics.rotation_angles += rotation_angles
 
-
-    number_of_be_ancillae = np.ceil(np.log2(L)) + B 
-    metrics.number_of_elbows += L - 1 # number of left elbows from indexing 
+    number_of_be_ancillae = np.ceil(np.log2(L)) + B
+    metrics.number_of_elbows += L - 1  # number of left elbows from indexing
 
     return metrics, rescaling_factor, number_of_be_ancillae

tests/test_bosonic.py
@@ -104,7 +104,7 @@ def test_bosonic_product_block_encoding(
         np.ceil(np.log2(maximum_occupation_number + 1))
         + max(int(np.log2(maximum_occupation_number + 1)) - 1, 0)
     )
-    
+
     assert metrics.number_of_nonclifford_rotations <= number_of_active_modes * (
         maximum_occupation_number + 3
     )
@@ -205,7 +205,7 @@ def test_bosonic_product_plus_hc_block_encoding(
     assert metrics.number_of_nonclifford_rotations <= number_of_active_modes * (
         maximum_occupation_number + 3
     )
-    
+
     assert len(metrics.rotation_angles) == number_of_active_modes * (
         maximum_occupation_number + 3
     )
@@ -277,7 +277,7 @@ def test_phi4_term(term):
     assert metrics.number_of_nonclifford_rotations <= number_of_active_modes * (
         maximum_occupation_number + 3
     )
-    
+
     assert len(metrics.rotation_angles) == number_of_active_modes * (
         maximum_occupation_number + 3
     )

tests/test_hamiltonian_metrics.py
@@ -14,6 +14,8 @@ from src.lobe.metrics import CircuitMetrics
 from src.lobe.system import System
 from src.lobe.interaction import _determine_block_encoding_function
 from src.lobe.index import index_over_terms
+from src.lobe.rescale import rescale_coefficients
+
 
 def count_metrics_numeric(operator, max_bosonic_occupancy: int = 1):
     groups = operator.group()
@@ -23,7 +25,9 @@ def count_metrics_numeric(operator, max_bosonic_occupancy: int = 1):
         translated_groups = []
         max_fermionic_mode = operator.max_fermionic_mode
         for term in groups:
-            translated_groups.append(translate_antifermions_to_fermions(term, max_fermionic_mode))
+            translated_groups.append(
+                translate_antifermions_to_fermions(term, max_fermionic_mode)
+            )
         groups = translated_groups
         operator = sum(groups, ParticleOperator())
 
@@ -54,12 +58,19 @@ def count_metrics_numeric(operator, max_bosonic_occupancy: int = 1):
 
     block_encoding_functions = []
     rescaling_factors = []
+    coefficients = []
     for term in groups:
         be_func, rescaling_factor = _determine_block_encoding_function(
             term, system, block_encoding_ancillae, clean_ancillae=clean_ancillae
         )
         block_encoding_functions.append(be_func)
         rescaling_factors.append(rescaling_factor)
+        coefficients.append(np.abs(term.coeffs[0]))
+
+    _, overall_rescaling_factor = rescale_coefficients(
+        coefficients,
+        rescaling_factors,
+    )
 
     metrics = CircuitMetrics()
 
@@ -72,103 +83,146 @@ def count_metrics_numeric(operator, max_bosonic_occupancy: int = 1):
 
     metrics += _metrics
 
-    rescaling_factor = sum(rescaling_factors)
     L = len(groups)
 
     number_of_be_ancillae = np.ceil(np.log2(L)) + number_of_block_encoding_anillae
 
-    return metrics, rescaling_factor, number_of_be_ancillae
+    return metrics, overall_rescaling_factor, number_of_be_ancillae
 
 
 def test_numeric_and_analytic_LOBE_counts_fermionic_product_of_number_ops():
-    operator = ParticleOperator('b1^ b1 b2^ b2') + ParticleOperator('b0^ b0')
-    
-    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = count_metrics_analytic(operator)
+    operator = ParticleOperator("b1^ b1 b2^ b2") + ParticleOperator("b0^ b0")
 
-    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(operator)
+    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = (
+        count_metrics_analytic(operator)
+    )
 
-    assert analytic_metrics == numeric_metrics
-    assert np.isclose(analytic_rescaling_factor, numeric_rescaling_factor)
-    assert np.isclose(analytic_n_be_anc, numeric_n_be_anc)
-
-@pytest.mark.parametrize('maximum_occupation', [1, 3, 7])
-def test_numeric_and_analytic_LOBE_counts_bosonic_product_of_number_ops(maximum_occupation):
-    operator = ParticleOperator('a0^ a0 a1^ a1^ a1^ a1 a1 a1 a2^ a2 a3^ a3') +\
-          ParticleOperator('a0^ a0^ a0 a0')
-    
-    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = count_metrics_analytic(operator)
-
-    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(operator)
+    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(
+        operator
+    )
 
     assert analytic_metrics == numeric_metrics
     assert np.isclose(analytic_rescaling_factor, numeric_rescaling_factor)
     assert np.isclose(analytic_n_be_anc, numeric_n_be_anc)
 
-@pytest.mark.parametrize('maximum_occupation', [1, 3, 7])
+
+@pytest.mark.parametrize("maximum_occupation", [1, 3, 7])
+def test_numeric_and_analytic_LOBE_counts_bosonic_product_of_number_ops(
+    maximum_occupation,
+):
+    operator = ParticleOperator(
+        "a0^ a0 a1^ a1^ a1^ a1 a1 a1 a2^ a2 a3^ a3"
+    ) + ParticleOperator("a0^ a0^ a0 a0")
+
+    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = (
+        count_metrics_analytic(operator, maximum_occupation)
+    )
+
+    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(
+        operator, maximum_occupation
+    )
+
+    assert analytic_metrics == numeric_metrics
+    assert np.isclose(analytic_rescaling_factor, numeric_rescaling_factor)
+    assert np.isclose(analytic_n_be_anc, numeric_n_be_anc)
+
+
+@pytest.mark.parametrize("maximum_occupation", [1, 3, 7])
 def test_numeric_and_analytic_LOBE_counts_product_of_number_ops(maximum_occupation):
-    operator = ParticleOperator('b0^ b0 a0^ a0') + ParticleOperator('a0^ a0 a1^ a1')
-    
-    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = count_metrics_analytic(operator)
+    operator = ParticleOperator("b0^ b0 a0^ a0") + ParticleOperator("a0^ a0 a1^ a1")
 
-    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(operator)
+    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = (
+        count_metrics_analytic(operator, maximum_occupation)
+    )
+
+    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(
+        operator, maximum_occupation
+    )
 
     assert analytic_metrics == numeric_metrics
     assert np.isclose(analytic_rescaling_factor, numeric_rescaling_factor)
     assert np.isclose(analytic_n_be_anc, numeric_n_be_anc)
+
 
 def test_numeric_and_analytic_LOBE_counts_nondiagonal_fermion():
-    operator = ParticleOperator('b0^ b1') + ParticleOperator('b0^ b1').dagger()
-    operator += ParticleOperator('b2^ b3') + ParticleOperator('b2^ b3').dagger()
-    operator += ParticleOperator('b0^ b1 b0 b1^') + ParticleOperator('b0^ b1 b0 b1^').dagger()
+    operator = ParticleOperator("b0^ b1") + ParticleOperator("b0^ b1").dagger()
+    operator += ParticleOperator("b2^ b3") + ParticleOperator("b2^ b3").dagger()
+    operator += (
+        ParticleOperator("b0^ b1 b0 b1^") + ParticleOperator("b0^ b1 b0 b1^").dagger()
+    )
 
-    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = count_metrics_analytic(operator)
+    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = (
+        count_metrics_analytic(operator)
+    )
 
-    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(operator)
+    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(
+        operator
+    )
 
     assert analytic_metrics == numeric_metrics
     assert np.isclose(analytic_rescaling_factor, numeric_rescaling_factor)
     assert np.isclose(analytic_n_be_anc, numeric_n_be_anc)
 
-    
 
-@pytest.mark.parametrize('maximum_occupation', [1, 3, 7])
+@pytest.mark.parametrize("maximum_occupation", [1, 3, 7])
 def test_numeric_and_analytic_LOBE_counts_nondiagonal_boson(maximum_occupation):
-    operator = ParticleOperator('a0^ a1') + ParticleOperator('a0^ a1').dagger()
-    operator += ParticleOperator('a2^ a3') + ParticleOperator('a2^ a3').dagger()
-    operator += ParticleOperator('a0^ a0^ a1 a1') + ParticleOperator('a0^ a0^ a1 a1').dagger()
+    operator = 2 * ParticleOperator("a0^ a1") + 2 * ParticleOperator("a0^ a1").dagger()
+    operator += ParticleOperator("a2^ a3") + ParticleOperator("a2^ a3").dagger()
+    operator += (
+        ParticleOperator("a0^ a0^ a1 a1") + ParticleOperator("a0^ a0^ a1 a1").dagger()
+    )
 
-    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = count_metrics_analytic(operator)
+    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = (
+        count_metrics_analytic(operator, maximum_occupation)
+    )
 
-    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(operator)
+    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(
+        operator, maximum_occupation
+    )
 
     assert analytic_metrics == numeric_metrics
     assert np.isclose(analytic_rescaling_factor, numeric_rescaling_factor)
     assert np.isclose(analytic_n_be_anc, numeric_n_be_anc)
 
-@pytest.mark.parametrize('maximum_occupation', [1, 3, 7])
+
+@pytest.mark.parametrize("maximum_occupation", [1, 3, 7])
 def test_numeric_and_analytic_LOBE_counts_interaction(maximum_occupation):
-    operator = ParticleOperator('b1^ b0 a1') + ParticleOperator('b1^ b0 a1').dagger()
-    operator += ParticleOperator('b0^ b1 a0') + ParticleOperator('b0^ b1 a0').dagger()
-    
-    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = count_metrics_analytic(operator)
+    operator = (
+        0.15 * ParticleOperator("b1^ b0 a1")
+        + 0.15 * ParticleOperator("b1^ b0 a1").dagger()
+    )
+    operator += ParticleOperator("b0^ b1 a0") + ParticleOperator("b0^ b1 a0").dagger()
 
-    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(operator)
+    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = (
+        count_metrics_analytic(operator, maximum_occupation)
+    )
+
+    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(
+        operator, maximum_occupation
+    )
 
     assert analytic_metrics == numeric_metrics
     assert np.isclose(analytic_rescaling_factor, numeric_rescaling_factor)
     assert np.isclose(analytic_n_be_anc, numeric_n_be_anc)
 
 
-
-@pytest.mark.parametrize("max_occupation", [1, 3, 7])
-def test_numeric_and_analytic_LOBE_counts_arbitrary_operator(max_occupation):
+@pytest.mark.parametrize("maximum_occupation", [1, 3, 7])
+def test_numeric_and_analytic_LOBE_counts_arbitrary_operator(maximum_occupation):
     operator = yukawa_hamiltonian(2, 1, 1, 1)
-    operator += ParticleOperator('b0^ b1^ b0 b1') + ParticleOperator('b0^ b1^ b0 b1').dagger()
-    operator += ParticleOperator('a0^ a1^ a0 a1') + ParticleOperator('a0^ a1^ a0 a1').dagger()
-    
-    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = count_metrics_analytic(operator)
+    operator += (
+        ParticleOperator("b0^ b1^ b0 b1") + ParticleOperator("b0^ b1^ b0 b1").dagger()
+    )
+    operator += (
+        ParticleOperator("a0^ a1^ a0 a1") + ParticleOperator("a0^ a1^ a0 a1").dagger()
+    )
 
-    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(operator)
+    analytic_metrics, analytic_rescaling_factor, analytic_n_be_anc = (
+        count_metrics_analytic(operator, maximum_occupation)
+    )
+
+    numeric_metrics, numeric_rescaling_factor, numeric_n_be_anc = count_metrics_numeric(
+        operator, maximum_occupation
+    )
 
     assert analytic_metrics == numeric_metrics
     assert np.isclose(analytic_rescaling_factor, numeric_rescaling_factor)